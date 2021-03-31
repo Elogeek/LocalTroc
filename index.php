bf84806ca8291ed7c86c01949ca018fc010fa145
elogeek <elodiechristin@gmail.com>
2021-03-31 19:40:34 +0200

    go

--- index.php
+++ index.php
@@ -1,1 +1,30 @@
 <?php
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <div class="nav-section">
+            <img id="logo" src="assets/img/logo.png" alt="logoSite">
+            <div> services</div>
+            <div> A propros</div>
+            <div>Recherche rapide</div>
+            <div>blog</div>
+            <div> partager</div>
+            <div>se connecter</div>
+        </div>
+    </header>
+    <section>
+        <div class="section1"></div>
+    </section>
+
+</body>
+</html>
