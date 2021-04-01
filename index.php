cda1a1f51c450c3f6da60871e0483ef29336bd75
elogeek <elodiechristin@gmail.com>
2021-04-01 13:50:55 +0200

    go

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
     <meta charset="UTF-8">
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <script src="https://kit.fontawesome.com/e3ddf954eb.js" crossorigin="anonymous"></script>
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="style.css">
     <title>Document</title>
@@ -23,8 +24,44 @@
         </div>
     </header>
     <section>
-        <div class="section1"></div>
+        <div class="section1">
+            un  mini carrousel où vidéo rapide?!
+            <button class="btn" type="button">M'inscrire</button>
+        </div>
     </section>
 
+    <h2>Qu'est-ce-que LocalTroc ?</h2>
+    <div class="font">
+        <i class="fas fa-users"></i>
+        <i class="fas fa-comments"></i>
+        <i class="fas fa-map"></i>
+        <i class="fas fa-euro-sign"></i>
+    </div>
+
+    <div class="text">
+        <div>Communauté</div>
+        <div>Blog</div>
+        <div>Local</div>
+        <div>GRATUIT</div>
+    </div>
+   <div id="section2">
+       <button class="btn" type="button">M'inscrire</button>
+   </div>
+  <div id="section3">
+      UNE GROSSE BARRE DE RECHERCHE ICI ;)
+  </div>
+  <div id="section4">
+      <span>Envie d'essayer?</span>
+      <button class="btn" type="button">M'inscrire</button>
+  </div>
+  <div>
+      article ici je sais pas encore quoi ;)
+      derniers services réalisés avec photo
+
+  </div>
+ <footer>
+     Footer ( lien vers réseaux)
+     <span>By Elogeek</span>
+ </footer>
 </body>
 </html>
