886b46a0a8cd910a91ebcbeed860eea6590944d5
elogeek <elodiechristin@gmail.com>
2021-05-11 16:53:32 +0200

    debbbug carousel :(

--- View/_partials/menu.php
+++ View/_partials/menu.php
@@ -1,55 +1,51 @@
 <header>
+    <!--forcer l'user a évaluer le services et l'user service comme vinted par expl après chaque service === AVIS (*****)-->
     <!--main menu-->
     <div class="nav-section">
         <img id="logo" src="/assets/img/logo.png" alt="logoSite">
-        <div> Services
-            <!--for catégorie--
-               enfants/chien ====> babysitters
-               couture ====> masques/vêtements
-               jardinage ====> tonte/désherbage
-               convoiturage ===>
-               déménagement ====> aide camionette/camionette déchets
-               cuisine ===> aide recette
-               course
-               photographie ===>événements
-               bricolage ====> réparation plomberie/...
-               autre ===>-->
+        <div id="menuService"> Services
+            <!--for catégorie-->
+        <a href="#"  title="for category"> Paramètres services</a>
+            <div id="nav_service">
+                <a href="#" title="itemMenu"> Rédiger une annonce</a <!--ok photo-->
+                <a href="#" title="itemMenu"> Rendre un service</a
+                <a href="#" title="itemMenu"> Modifier une annonce</a
+                <a href="#" title="itemMenu"> Supprimer une annonce</a
+                <a href="#" title="itemMenu"> Signaler un service malveillant</a
+
+
+            </div>
         </div>
         <div> A propros
-            <!--Infos légales
-                petite intro du site...(où nous trouver)
-                Cookies
-                Loi data user
-                charte à faire signer lors d'envoi du  code compte client lors de la vérif
-              -->
+            <div id="nav_about">
+                <a href="#" title="itemMenu"> Informations légales</a
+                <a href="#" title="itemMenu"> Où nous trouver?</a <!--bref intro du site-->
+                <a href="#" title="itemMenu"> Ce site utilise des cookies</a
+                <a href="#" title="itemMenu"> Protections des données utilisateurs</a
+                <a href="#" title="itemMenu"> Rappel charte (réglements du site)</a <!--charte déjà signer leurs de l'envoie du code par mail-->
+            </div>
+
         </div>
-        <div>Recherche rapide</div> <!--redirection big search bar-->
+        <div>Recherche rapide</div> <!--redirection big search bar avec search le + proche (expl ta === tapis/table....)-->
         <div>Chat</div>
         <!--lorsque l'utilisateur fait une recherche service et trouve le bon, quand il clique sur une enveloppe message -->
         <div>Avis</div> <!--redirection bar statisfaction users-->
         <div>Partager</div> <!--redirection footer?-->
         <div>Se connecter
-            <!--paramètres du compte
-                 changer avatar
-                 changer d'email
-                 changer de numéro de téléphone
-                 changer pseudo
-                 supprimer le compte
+            <a href="#" title="spaceUser">Paramètres du compte</a>
+            <div id="nav_spaceUser">
+                <a href="#" title="itemMenu"> Changer d'avatar</a
+                <a href="#" title="itemMenu"> Changer d'adresse email</a
+                <a href="#" title="itemMenu"> Changer de numéro de téléphone</a
+                <a href="#" title="itemMenu"> Changer de pseudo</a
+                <a href="#" title="itemMenu"> Supprimer le compte</a
+                <a href="#" title="itemMenu"> Voir mes évaluations</a
+                <a href="#" title="itemMenu"> Recevoir mes étoiles</a <!--now/+later-->
+                <a href="#" title="itemMenu"> Mes avis rédigées</a
+                <a href="#" title="itemMenu"> Signaler un membre malveillant</a
+            </div>
 
-                 voir mes évaluations/avis
-                 les avis rédigés
-
-                  forcer l'user a évaluer le services et l'user service comme vinted par expl après chaque service === AVIS (*****)
-
-                paramètres services
-                   modifier l'annonce
-                   supprimer l'annonce
-                   ajouter une annonce ( on peut mettre des photos dedans)
-                   recevoir mes étoiles === échange de service ( choix : mtnt / où plus tard)
-
-                paramètre user
-                    signaler un menbre qui ne respecte pas le site
-            -->
         </div>
+
     </div>
 </header>
--- index.php
+++ index.php
@@ -6,66 +6,63 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <script src="https://kit.fontawesome.com/e3ddf954eb.js" crossorigin="anonymous"></script>
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="./assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/style.css">
     <title>Document</title>
 </head>
 <body>
+
 <header>
+    <!--forcer l'user a évaluer le services et l'user service comme vinted par expl après chaque service === AVIS (*****)-->
     <!--main menu-->
     <div class="nav-section">
-        <img id="logo" src="assets/img/logo.png" alt="logoSite">
-        <div> Services
-            <!--for catégorie--
-               enfants/chien ====> babysitters
-               couture ====> masques/vêtements
-               jardinage ====> tonte/désherbage
-               convoiturage ===>
-               déménagement ====> aide camionette/camionette déchets
-               cuisine ===> aide recette
-               course
-               photographie ===>événements
-               bricolage ====> réparation plomberie/...
-               autre ===>-->
+        <img id="logo" src="/assets/img/logo.png" alt="logoSite">
+        <div id="menuService"> Services
+            <!--for catégorie-->
+            <a href="#"  title="for category"> Paramètres services</a>
+            <div id="nav_service">
+                <a href="#" title="itemMenu"> Rédiger une annonce</a <!--ok photo-->
+                <a href="#" title="itemMenu"> Rendre un service</a
+                <a href="#" title="itemMenu"> Modifier une annonce</a
+                <a href="#" title="itemMenu"> Supprimer une annonce</a
+                <a href="#" title="itemMenu"> Signaler un service malveillant</a
+
+
+            </div>
         </div>
         <div> A propros
-            <!--Infos légales
-                petite intro du site...(où nous trouver)
-                Cookies
-                Loi data
-              -->
+            <div id="nav_about">
+                <a href="#" title="itemMenu"> Informations légales</a
+                <a href="#" title="itemMenu"> Où nous trouver?</a <!--bref intro du site-->
+                <a href="#" title="itemMenu"> Ce site utilise des cookies</a
+                <a href="#" title="itemMenu"> Protections des données utilisateurs</a
+                <a href="#" title="itemMenu"> Rappel charte (réglements du site)</a <!--charte déjà signer leurs de l'envoie du code par mail-->
+            </div>
+
         </div>
-        <div>Recherche rapide</div> <!--redirection big search bar-->
+        <div>Recherche rapide</div> <!--redirection big search bar avec search le + proche (expl ta === tapis/table....)-->
         <div>Chat</div>
         <!--lorsque l'utilisateur fait une recherche service et trouve le bon, quand il clique sur une enveloppe message -->
         <div>Avis</div> <!--redirection bar statisfaction users-->
         <div>Partager</div> <!--redirection footer?-->
         <div>Se connecter
-            <!--paramètres du compte
-                 changer avatar
-                 changer d'email
-                 changer de numéro de téléphone
-                 changer pseudo
-                 supprimer le compte
+            <a href="#" title="spaceUser">Paramètres du compte</a>
+            <div id="nav_spaceUser">
+                <a href="#" title="itemMenu"> Changer d'avatar</a
+                <a href="#" title="itemMenu"> Changer d'adresse email</a
+                <a href="#" title="itemMenu"> Changer de numéro de téléphone</a
+                <a href="#" title="itemMenu"> Changer de pseudo</a
+                <a href="#" title="itemMenu"> Supprimer le compte</a
+                <a href="#" title="itemMenu"> Voir mes évaluations</a
+                <a href="#" title="itemMenu"> Recevoir mes étoiles</a <!--now/+later-->
+                <a href="#" title="itemMenu"> Mes avis rédigées</a
+                <a href="#" title="itemMenu"> Signaler un membre malveillant</a
+            </div>
 
-                 voir mes évaluations/avis
-                 les avis rédigés
-
-                  forcer l'user a évaluer le services et l'user service comme vinted par expl après chaque service === AVIS (*****)
-
-                paramètres services
-                   modifier l'annonce
-                   supprimer l'annonce
-                   ajouter une annonce ( on peut mettre des photos dedans)
-                   recevoir mes étoiles === échange de service ( choix : mtnt / où plus tard)
-
-                paramètre user
-                    signaler un menbre qui ne respecte pas le site
-            -->
         </div>
+
     </div>
 </header>
 
-<!--envoyer un code pour confirmer la compte lors de l'inscription-->
     <section>
         <div class="section1">
             <!--videoImg-->
@@ -122,57 +119,57 @@
 
               <div class="item">
                   <img class="imgArtcls" src="assets/img/jardinage.jpg" alt="jardin">
-                  <span class="item__description">Besoin d'aide en jardinerie? Trouver un troqueur/troqueuse.</span>
+                  <div class="item__description">Besoin d'aide en jardinerie? Trouver un troqueur/troqueuse.</div>
               </div>
 
               <div class="item">
                   <img class="imgArtcls" src="assets/img/dog-sitter.jpg" alt="dog-sitter">
-                  <span class="item__description">Personne pour garder ou promener son chien ? Parcourir nos annonces.</span>
+                  <div class="item__description">Personne pour garder ou promener son chien ? Parcourir nos annonces.</div>
               </div>
 
               <div class="item">
                   <img class="imgArtcls"  src="assets/img/couture.jpg" alt="couture">
-                  <span class="item__description">
+                  <div class="item__description">
                       Trop de connaissances en couture, envie de partager sa passion ?
                       LocalTroc est fait pour vous .
-                  </span>
+                  </div>
               </div>
 
               <div class="item">
                   <img class="imgArtcls" src="assets/img/photographe.jpg" alt="photographe">
-                  <span class="item__description">
+                  <div class="item__description">
                       Vous êtes bon photographe ? Vous chercher un bon photographe dans votre région?
                       Venez sur LocalTroc .
-                  </span>
+                  </div>
               </div>
 
               <div class="item">
                   <img class="imgArtcls" src="assets/img/course.jpg" alt="course">
-                  <span class="item__description">
+                  <div class="item__description">
                       Pas le temps de faire vos courses vous même? Vous chercher une personne de confiance pour les faire à votre place ?
                       Trouver un troqueur/ une troqueuse .
-                  </span>
+                  </div>
               </div>
 
               <div class="item">
                   <img class="imgArtcls" src="assets/img/baby-sitter.jpg" alt="baby-sitter">
-                  <span class="item__description">
+                  <div class="item__description">
                       Besoin de faire garder vos enfants ? Envie de garder les enfants de vos voisins?
                       Pratiquer le troc .
-                  </span>
+                  </div>
               </div>
 
               <div class="item">
                   <img class="imgArtcls" src="assets/img/déménagement.jpg" alt="demenagement">
-                  <span class="item__description">
+                  <div class="item__description">
                       Vous avez accumuler trop de chose ? Pas assez de place pour tout déménager ?
                       Rédiger une annonce sur LocalTroc .
-                  </span>
+                  </div>
               </div>
 
               <div class="item">
                   <img class="imgArtcls" src="assets/img/cuisine.jpg" alt="cuisine">
-                  <span class="item__description">
+                  <div class="item__description">
                       Vous avez beaucoup de connaissances en cuisine ? Envie de partager des recettes ?
                       LocalTroc est fait pour vous .
                   </span>
@@ -180,12 +177,15 @@
 
               <div class="item">
                   <img class="imgArtcls" src="assets/img/bricolage.jpg" alt="bricolage">
-                  <span class="item__description">
+                  <div class="item__description">
                       Vous êtes très bon bricoleur ? Vous chercher un bon bricoleur dans votre région ?
                       Pratiquer le troc .
-                  </span>
+                  </div>
               </div>
-
+              <div>
+                  <!--control carousel-->
+                  <div class="carousel__next"></div>
+                  <div class="carousel__prev"></div>
          </div>
      </div>
          <!-- BARRE DE SATISFACTION ET AVIS CLIENT HERE / bckgrd linear
