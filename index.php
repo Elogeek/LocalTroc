6b8db691ef489ad783c4cbb59bb51d17b1775bca
elogeek <elodiechristin@gmail.com>
2021-05-12 17:38:20 +0200

    DESIGN avis user, satisfaction users,...

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <!doctype html>
-<html lang="en">
+<html lang="en" xmlns="http://www.w3.org/1999/html">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport"
@@ -9,7 +9,7 @@
     <link rel="stylesheet" href="assets/css/style.css">
     <title>Document</title>
 </head>
-<body>
+<div>
 
     <!--forcer l'user a évaluer le services et l'user service comme vinted par expl après chaque service === AVIS (*****)-->
     <!--main menu-->
@@ -69,13 +69,13 @@
         <i class="fas fa-users"></i>
         <i class="fas fa-comments"></i>
         <i class="fas fa-map"></i>
-        <i class="fas fa-euro-sign"></i>
+        <i class="fas fa-piggy-bank"></i>
     </div>
 
     <div class="text">
-        <div>Communauté</div>
-        <div>Blog</div>
-        <div>Local</div>
+        <div>COMMUNAUTÉ</div>
+        <div>CHAT</div>
+        <div>LOCAL</div>
         <div>GRATUIT</div>
     </div>
 
@@ -95,7 +95,7 @@
          <div id="search-bar">
              <input type="text" placeholder=" Où ?">
              <input type="text" placeholder=" Quand ?">
-             <input type="text" placeholder=" Quoi?">
+             <input type="text" placeholder=" Quoi ?">
                 <button type="submit" name="header_search_submit" class="button-reset color-inherit db o-60 absolute center-v right-1 hover-primary6"><svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="search" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="svg-inline--fa fa-search fa-w-16 fa-fw"><path fill="currentColor" d="M505 442.7L405.3 343c-4.5-4.5-10.6-7-17-7H372c27.6-35.3 44-79.7 44-128C416 93.1 322.9 0 208 0S0 93.1 0 208s93.1 208 208 208c48.3 0 92.7-16.4 128-44v16.3c0 6.4 2.5 12.5 7 17l99.7 99.7c9.4 9.4 24.6 9.4 33.9 0l28.3-28.3c9.4-9.4 9.4-24.6.1-34zM208 336c-70.7 0-128-57.2-128-128 0-70.7 57.2-128 128-128 70.7 0 128 57.2 128 128 0 70.7-57.2 128-128 128z"></path></svg></button>
                 <path fill="currentColor" d="M505 442.7L405.3 343c-4.5-4.5-10.6-7-17-7H372c27.6-35.3 44-79.7 44-128C416 93.1 322.9 0 208 0S0 93.1 0 208s93.1 208 208 208c48.3 0 92.7-16.4 128-44v16.3c0 6.4 2.5 12.5 7 17l99.7 99.7c9.4 9.4 24.6 9.4 33.9 0l28.3-28.3c9.4-9.4 9.4-24.6.1-34zM208 336c-70.7 0-128-57.2-128-128 0-70.7 57.2-128 128-128 70.7 0 128 57.2 128 128 0 70.7-57.2 128-128 128z"></path>
          </div>
@@ -183,17 +183,105 @@
              </div>
 
          </div>
-    </body>
-         <!-- BARRE DE SATISFACTION ET AVIS CLIENT HERE / bckgrd linear
-            <img src="assets/img/expl.png" alt="expl">-->
+    </div>
+
+    <!-- BARRE DE SATISFACTION-->
+    <div id="section5">
+        <div id="happyBar">
+            <div id="textHappy">
+                <span>98%</span>
+                <span>d'utilisateurs satisfaits</span>
+                <div>
+                    <progress id="barHappy" max="20000" value="250"></progress>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div id="section6">
+        <!--AVIS CLIENT HERE-->
+        <div class="rectangle">
+            <div class="image"></div>
+            <div class="description">
+                <h2>Déménagement</h2>
+                <p>
+                    <span class="blue">Odile</span> a fait débarrasser des meubles à <span class="gras">Chimay(6460)</span> par <span class="blue">Georges</span>.
+                </p>
+            </div>
+            <div class="avis">
+                <div><hr class="lignes"></div>
+                 <div class="star">
+                     <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                 </div>
+                <div><hr class="lignes"></div>
+               <p>
+                   Très bon troqueur , il a répondu à mes attentes, de plus travail très propre, vite fait et bien fait
+                   merci beaucoup Georges!
+               </p>
+            </div>
+        </div>
+
+        <div class="rectangle">
+            <div class="image"></div>
+            <div class="description">
+                <h2>Déménagement</h2>
+                <p>
+                    <span class="blue">Odile</span> a fait débarrasser des meubles à <span class="gras">Chimay(6460)</span> par <span class="blue">Georges</span>.
+                </p>
+            </div>
+            <div class="avis">
+                <div><hr class="lignes"></div>
+                <div class="star">
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                </div>
+                <div><hr class="lignes"></div>
+                <p>
+                    Très bon troqueur , il a répondu à mes attentes, de plus travail très propre, vite fait et bien fait
+                    merci beaucoup Georges!
+                </p>
+            </div>
+        </div>
+        <div class="rectangle">
+            <div class="image"></div>
+            <div class="description">
+                <h2>Déménagement</h2>
+                <p>
+                    <span class="blue">Odile</span> a fait débarrasser des meubles à <span class="gras">Chimay(6460)</span> par <span class="blue">Georges</span>.
+                </p>
+            </div>
+            <div class="avis">
+                <div><hr class="lignes"></div>
+                <div class="star">
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                </div>
+                <div><hr class="lignes"></div>
+                <p>
+                    Très bon troqueur , il a répondu à mes attentes, de plus travail très propre, vite fait et bien fait
+                    merci beaucoup Georges!
+                </p>
+            </div>
+        </div>
+    </div>
 
 
-    <!--autre section ===> section5 ?
+    <!--section7 "helpContact" des users avec messages automatiques
+    besoin d'aide, un renseignement, parler avec les autres menbres connecter par expl-->
+
+   <!--autre section ===> section8 ?
     si assez de temps, services près de chez soi sur une carte (style google maps)
      avec les services en minuscule (icône), quand service terminée === hidden -->
-
-    <!--sous la carte ===> un onglet message qui serait le "helpContact" des users avec messages automatiques
-    besoin d'aide, un renseignement, parler avec les autres menbres connecter par expl-->
     <div id="helpContact">
         <i class="far fa-envelope"></i>
     </div>
