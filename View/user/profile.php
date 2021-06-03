84347b0dd7ea0f383ab790ed899443b821d1c508
Elogeek <elodiechristin@gmail.com>
2021-06-03 20:21:12 +0200

    Finished responsive profile.

--- View/user/profile.php
+++ View/user/profile.php
@@ -10,75 +10,74 @@ $userProfile = $params['userProfile'];
 ?>
 
 <div class="profile-container">
-    <!-- Edit buttons. -->
-    <div id="profile-actions">
-        <a href="#" title="item" class="btn btn-primary"><i class="far fa-edit"></i>Editer mes données</a>
-        <a href="#" title="item" class="btn btn-primary"><i class="far fa-edit"></i>Editer mon profil</a>
-    </div>
-
     <!--image avatar user-->
-    <div id="profile-content">
+    <div class="profile-content">
         <div class="imgAvatar">
             <img src="/assets/img/userProfile.webp" alt="Mon profile">
-            <div class="pseudoUser">
-                <!--pseudo-->
-                <span> <?= $userProfile->getPseudo() ?></span>
-            </div>
+            <span> <?= $userProfile->getPseudo() ?></span>
         </div>
 
-        <h1>Mon profil </h1>
-        <hr>
+        <div class="profile-table">
 
-        <!-- User basic information. -->
-        <h2>Vos informations de base</h2>
-        <table class="profile-table">
-            <tr>
-                <th>Nom</th>
-                <td><?= $user->getLastName() ?></td>
-            </tr>
-            <tr>
-                <th>Prénom</th>
-                <td><?= $user->getFirstName() ?></td>
-            </tr>
-            <tr>
-                <th>E-mail</th>
-                <td><?= $user->getEmail() ?></td>
-            </tr>
-        </table>
+            <!-- Edit buttons. -->
+            <div id="profile-actions">
+                <a href="#" title="item" class="btn btn-primary"><i class="far fa-edit"></i>Editer mes données</a>
+                <a href="#" title="item" class="btn btn-primary"><i class="far fa-edit"></i>Editer mon profil</a>
+            </div>
 
-        <!-- Profile information. -->
-        <h2>Vos informations de profile</h2>
-        <table class="profile-table">
-            <tr>
-                <th>Pseudo</th>
-                <td><?= $userProfile->getPseudo() ?></td>
-            </tr>
-            <tr>
-                <th>Téléphone</th>
-                <td><?= $userProfile->getPhone() ?></td>
-            </tr>
-            <tr>
-                <th>Anniversaire</th>
-                <td><?= $userProfile->getBirthday() ?></td>
-            </tr>
-            <tr>
-                <th>Adresse</th>
-                <td><?= $userProfile->getAddress() ?></td>
-            </tr>
-            <tr>
-                <th>Code postal</th>
-                <td><?= $userProfile->getCodeZip() ?></td>
-            </tr>
-            <tr>
-                <th>Ville</th>
-                <td><?= $userProfile->getCity() ?></td>
-            </tr>
-            <tr>
-                <th>Informations complémentaires</th>
-                <td><?= $userProfile->getMoreInfos() ?></td>
-            </tr>
-        </table>
+            <h1>Mon profil </h1>
+            <hr>
 
+            <!-- User basic information. -->
+            <h2>Vos informations de base</h2>
+            <table class="profile-table">
+                <tr>
+                    <th>Nom</th>
+                    <td><?= $user->getLastName() ?></td>
+                </tr>
+                <tr>
+                    <th>Prénom</th>
+                    <td><?= $user->getFirstName() ?></td>
+                </tr>
+                <tr>
+                    <th>E-mail</th>
+                    <td><?= $user->getEmail() ?></td>
+                </tr>
+            </table>
+
+            <!-- Profile information. -->
+            <h2>Vos informations de profil</h2>
+            <table class="profile-table">
+                <tr>
+                    <th>Pseudo</th>
+                    <td><?= $userProfile->getPseudo() ?></td>
+                </tr>
+                <tr>
+                    <th>Téléphone</th>
+                    <td><?= $userProfile->getPhone() ?></td>
+                </tr>
+                <tr>
+                    <th>Anniversaire</th>
+                    <td><?= $userProfile->getBirthday() ?></td>
+                </tr>
+                <tr>
+                    <th>Adresse</th>
+                    <td><?= $userProfile->getAddress() ?></td>
+                </tr>
+                <tr>
+                    <th>Code postal</th>
+                    <td><?= $userProfile->getCodeZip() ?></td>
+                </tr>
+                <tr>
+                    <th>Ville</th>
+                    <td><?= $userProfile->getCity() ?></td>
+                </tr>
+                <tr>
+                    <th>Informations complémentaires</th>
+                    <td><?= $userProfile->getMoreInfos() ?></td>
+                </tr>
+            </table>
+        </div>
     </div>
 
 </div>
