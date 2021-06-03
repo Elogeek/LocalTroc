c29abe4c4db4229b07e1ecb7108254e6f4766d7a
Elogeek <elodiechristin@gmail.com>
2021-06-03 11:32:25 +0200

    Profile par défaut, débugg design home,...

--- View/register.php
+++ View/register.php
@@ -19,44 +19,23 @@ if(isset($success)) { ?>
 
         <!-- first name and last name -->
         <input type="text" name="firstname" id="firstname" placeholder="Prénom" required>
-        <div class="invalid-feedback">Veuillez entrer un prénom valide</div>
-
         <input type="text" name="lastname" id="lastname" placeholder="Nom" required>
-        <div class="invalid-feedback">Veuillez entrer un nom valide</div>
-
         <input type="text" name="pseudo" id="pseudo" placeholder="Pseudo" required>
-        <div class="invalid-feedback">Veuillez entrer votre pseudo</div>
 
         <!-- User infos. -->
 
         <input type="date" name="birthday">
-
         <input name="city" type="text" placeholder="Ville" required>
-        <div class="invalid-feedback">Veuillez entrer une ville valide</div>
-
         <input name="address" type="text" placeholder="Adresse" required>
-        <div class="invalid-feedback">Veuillez entrer une adresse valide</div>
-
         <input name="codeZip" type="number" placeholder="Code postal" required>
-        <div class="invalid-feedback">Veuillez entrer un code postal valide</div>
-
-
         <input name="other" type="text" placeholder="Autre : diplôme, passions, compétences,...">
-
         <input name="phone" type="tel" placeholder="Téléphone">
-        <div class="invalid-feedback">Veuillez entrer un numéro de téléphone valide au format: +33...</div>
-
 
         <!-- User info connect -->
         <input name="mail" type="email" placeholder="Adresse mail" required>
-        <div class="invalid-feedback">Veuillez entrer une adresse mail valide</div>
-
         <input type="password" name="password" id="password" placeholder="Mot de passe" required>
         <small>Plus grand que 5 caractères, lettres, majuscules, chiffres </small>
-        <div class="invalid-feedback">Veuillez entrer un mot de passe valide</div>
-
         <input type="password" name="passwordConfirm" id="passwordConfirm" placeholder="Répétez le mot de passe" required>
-        <div class="invalid-feedback">Les deux mots de passe ne correspondent pas</div>
 
         <input type="submit" name="submit" value="Créer mon compte">
     </form>
