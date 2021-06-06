501fbbb1760fa07e1d94a660709fbca3b68f844c
Elogeek <elodiechristin@gmail.com>
2021-06-06 09:38:14 +0200

    Added profile details into user profile menu.

--- View/user/_partials/user-profile-menu.php
+++ View/user/_partials/user-profile-menu.php
@@ -1,6 +1,10 @@
 <nav>
     <ul>
-        <li></li>
+        <li>
+            <a href="/index.php?controller=user&action=profile" title="Mon profil">
+                <i class="fas fa-user-alt"></i>Mon profil
+            </a>
+        </li>
         <li>
             <a href="/index.php?controller=user&action=editProfile" title="Editer profil">
                 <i class="fas fa-user-edit"></i>Editer mon profil
