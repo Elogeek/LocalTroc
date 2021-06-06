749539732c64de8e9a61aa27ee1a75797ada50d0
Elogeek <elodiechristin@gmail.com>
2021-06-06 16:49:10 +0200

    Removed unneeded classes.

--- Controller/opinionServiceController.php
+++ Controller/opinionServiceController.php
@@ -1,23 +1,5 @@
 <?php
 
-class opinionServiceController {
-
-public function goOpinion() {
-$username = "John";
-require_once "./include.php";
-require_once './View/user/opinion.php';
-}
-
-}
-
-
-//check a dossier user exist, and create if not exist (check one * et pas 2 in the boucle)
-if (!is_dir("uploads")) {
-    mkdir('uploads','0755');
-}
-//type file accept
-$mineTypes = ["text/plain", "image/jpeg", "image/png", "image/jpg"];
-
 //récupération an file/files user
 require_once  $_SERVER['DOCUMENT_ROOT'] . '/Classes/FileUpload.php';
 
@@ -25,3 +7,4 @@ $fileUploader = new FileUpload($_FILES['avatar'], $_SERVER['DOCUMENT_ROOT'] . '/
 if($fileUploader->isSizeInThreshold()) {
     $result = $fileUploader->upload(); // succes ou échec.
 }
+
