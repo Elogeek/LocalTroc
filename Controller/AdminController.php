bcc57f5584721df8fc9d68732d1e947efde8cc46
Elogeek <elodiechristin@gmail.com>
2021-06-07 20:42:11 +0200

    Started admin area ( admin menu ).

--- Controller/AdminController.php
+++ Controller/AdminController.php
@@ -3,11 +3,4 @@
 
 class AdminController extends Controller {
 
-    public function goToAdmin()
-    {
-        $username = "Elodie";
-        require_once "../include.php";
-        require_once './View/admin/pageAdmin.php';
-        $this->showView(  'admin/pageAdmin', [], [], ['pageAdmin']);
-    }
 }
