f7d77e917b539ca8753a97d3e07f6dbc69a081d0
Elogeek <elodiechristin@gmail.com>
2021-06-07 06:40:31 +0200

    Removed unessecary template.

--- View/user/_partials/avatar-and-menu.php
+++ View/user/_partials/avatar-and-menu.php
@@ -15,6 +15,35 @@
     <span> <?= $userProfile->getPseudo() ?></span>
 
     <div class="user-profile-menu">
-        <?php include_once $_SERVER['DOCUMENT_ROOT'] . '/View/user/_partials/user-profile-menu.php' ?>
+        <nav>
+            <ul>
+                <li>
+                    <a href="/index.php?controller=service&action=user-services" title="Mes services">
+                        <i class="fas fa-briefcase"></i>Mes services
+                    </a>
+                </li>
+
+                <li>
+                    <a href="/index.php?controller=user&action=profile" title="Mon profil">
+                        <i class="fas fa-user-alt"></i>Mon profil
+                    </a>
+                </li>
+                <li>
+                    <a href="/index.php?controller=user&action=editProfile" title="Editer profil">
+                        <i class="fas fa-user-edit"></i>Editer mon profil
+                    </a>
+                </li>
+                <li>
+                    <a href="/index.php?controller=user&action=editInformation" title="Editer mes informations">
+                        <i class="fas fa-edit"></i>Editer mes informations
+                    </a>
+                </li>
+                <li>
+                    <a id="delete-user" href="/index.php?controller=user&action=delete" title="Editer mes informations">
+                        <i class="fas fa-trash"></i>Supprimer mon compte
+                    </a>
+                </li>
+            </ul>
+        </nav>
     </div>
 </div>
