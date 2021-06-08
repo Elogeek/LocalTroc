6cc0732ed79a7d5afd4540292112e8096d6569b4
Elogeek <elodiechristin@gmail.com>
2021-06-08 09:19:27 +0200

    change design service home page

--- View/home.php
+++ View/home.php
@@ -33,19 +33,6 @@ if(!$connected) { ?>
     </div><?php
 } ?>
 
-
-
-<div id="div4">
-    <h2>Ma recherche rapide !</h2>
-    <div id="search-bar">
-        <input type="text" placeholder=" Où ?">
-        <input type="text" placeholder=" Quand ?">
-        <input type="text" placeholder=" Quoi ?">
-        <button type="submit" name="header_search_submit" class="button-reset color-inherit db o-60 absolute center-v right-1 hover-primary6"><svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="search" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="svg-inline--fa fa-search fa-w-16 fa-fw"><path fill="currentColor" d="M505 442.7L405.3 343c-4.5-4.5-10.6-7-17-7H372c27.6-35.3 44-79.7 44-128C416 93.1 322.9 0 208 0S0 93.1 0 208s93.1 208 208 208c48.3 0 92.7-16.4 128-44v16.3c0 6.4 2.5 12.5 7 17l99.7 99.7c9.4 9.4 24.6 9.4 33.9 0l28.3-28.3c9.4-9.4 9.4-24.6.1-34zM208 336c-70.7 0-128-57.2-128-128 0-70.7 57.2-128 128-128 70.7 0 128 57.2 128 128 0 70.7-57.2 128-128 128z"></path></svg></button>
-        <path fill="currentColor" d="M505 442.7L405.3 343c-4.5-4.5-10.6-7-17-7H372c27.6-35.3 44-79.7 44-128C416 93.1 322.9 0 208 0S0 93.1 0 208s93.1 208 208 208c48.3 0 92.7-16.4 128-44v16.3c0 6.4 2.5 12.5 7 17l99.7 99.7c9.4 9.4 24.6 9.4 33.9 0l28.3-28.3c9.4-9.4 9.4-24.6.1-34zM208 336c-70.7 0-128-57.2-128-128 0-70.7 57.2-128 128-128 70.7 0 128 57.2 128 128 0 70.7-57.2 128-128 128z"></path>
-    </div>
-</div>
-
 <!--articles diapo-->
 <div class="diapo">
     <h2 id="titleDiapo">Besoin d'aide ? Rien à faire, envie de partager son temps, sa passion... </h2>
@@ -216,38 +203,44 @@ if(!$connected) { ?>
 <div id="div9">
     <!--Affiche près de chez soi, les derniers services create
   quand service terminée === hidden -->
-    <div class="helpRectangle">
-        <div class="imageRectangle"></div>
-        <div class="subjectRectangle">
+    <div class="lastSerivces">
+        <div class="imageRectangle"></div> <!--soit linux où avatar de l' user qui créer le service-->
+        <div class="subjectServices">
             <h2>Course</h2>
+            <span>Paru le date here</span>
+            <hr>
         </div>
-        <div class="descriptionRectangle">
+        <div class="descriptionServices">
             <p>
-                <span class="blue">John</span> a besoin d'aide pour faire ses courses  à <span class="gras"> Bourlers (6460) </span> <span class="timeService">aujourd'hui .</span>
+                <span>John</span> a besoin d'aide pour faire ses courses  à <span> Bourlers (6460) . </span>
             </p>
         </div>
     </div>
 
-    <div class="helpRectangle" id="rect2">
-        <div class="imageRectangle"></div>
-        <div class="subjectRectangle">
+    <div class="lastSerivces">
+        <div class="imageRectangle"></div> <!--soit linux où avatar de l' user qui créer le service-->
+        <div class="subjectServices">
             <h2>Course</h2>
+            <span>Paru le date here</span>
+            <hr>
         </div>
-        <div class="descriptionRectangle">
+        <div class="descriptionServices">
             <p>
-                <span class="blue">John</span> a besoin d'aide pour faire ses courses  à <span class="gras"> Bourlers (6460) </span> <span class="timeService">aujourd'hui .</span>
+                <span>John</span> a besoin d'aide pour faire ses courses  à <span> Bourlers (6460) . </span>
             </p>
         </div>
     </div>
 
-    <div class="helpRectangle">
-        <div class="imageRectangle"></div>
-        <div class="subjectRectangle">
+    <div class="lastSerivces">
+        <div class="imageRectangle"></div> <!--soit linux où avatar de l' user qui créer le service-->
+        <div class="subjectServices">
             <h2>Course</h2>
+            <span>Paru le date here</span>
+            <hr>
         </div>
-        <div class="descriptionRectangle">
+        <div class="descriptionServices">
             <p>
-                <span class="blue">John</span> a besoin d'aide pour faire ses courses  à <span class="gras"> Bourlers (6460) </span> <span class="timeService">aujourd'hui .</span>
+                <span>John</span> a besoin d'aide pour faire ses courses  à <span> Bourlers (6460) . </span>
             </p>
         </div>
     </div>
